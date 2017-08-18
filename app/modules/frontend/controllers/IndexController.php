5510ddda1b5918165d9731890234cbb78933fc1f
gzp <gzp@goozp.com>
2017-08-18 18:22:15 +0800

    基本布局

--- app/modules/frontend/controllers/IndexController.php
+++ app/modules/frontend/controllers/IndexController.php
@@ -5,23 +5,14 @@ namespace ZPhal\Modules\Frontend\Controllers;
 class IndexController extends ControllerBase
 {
 
+    public function initialize()
+    {
+        //$this->view->setTemplateAfter("common");
+    }
+
     public function indexAction()
     {
-        /**
-         * HTML 头部资源
-         */
-        $headerCollection = $this->assets->collection("header");
-        $headerCollection->addCss("themes/default/public/library/bootstrap/css/bootstrap.min.css", false,  false); // Bootstrap
-        $headerCollection->addCss("themes/default/public/css/cover.css", false,  false); // Bootstrap
 
-        /**
-         * HTML尾部资源
-         */
-        $footerCollection = $this->assets->collection("footer");
-        $footerCollection->addJs("themes/default/public/js/vendor/jquery-slim.min.js", false,  false);; // jQuery 3
-        $footerCollection->addJs("themes/default/public/js/vendor/popper.min.js", false,  false);;
-        $footerCollection->addJs("themes/default/public/library/bootstrap/js/bootstrap.min.js", false,  false);;
-        $footerCollection->addJs("themes/default/public/js/ie10-viewport-bug-workaround.js", false,  true);;
     }
 
 }
