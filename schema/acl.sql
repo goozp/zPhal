258cacb2986baa3ab7e18dcd4f089e06fdec65b8
gzp <gzp@goozp.com>
2017-09-06 18:24:30 +0800

    acl sql

---
+++ schema/acl.sql
@@ -0,0 +1,61 @@
+
+CREATE TABLE IF NOT EXISTS `zp_permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `profilesId` int(10) unsigned NOT NULL,
+  `resource` varchar(16) NOT NULL,
+  `action` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `profilesId` (`profilesId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `profilesId`, `resource`, `action`) VALUES
+(1, 3, 'users', 'index'),
+(2, 3, 'users', 'search'),
+(3, 3, 'profiles', 'index'),
+(4, 3, 'profiles', 'search'),
+(5, 1, 'users', 'index'),
+(6, 1, 'users', 'search'),
+(7, 1, 'users', 'edit'),
+(8, 1, 'users', 'create'),
+(9, 1, 'users', 'delete'),
+(10, 1, 'users', 'changePassword'),
+(11, 1, 'profiles', 'index'),
+(12, 1, 'profiles', 'search'),
+(13, 1, 'profiles', 'edit'),
+(14, 1, 'profiles', 'create'),
+(15, 1, 'profiles', 'delete'),
+(16, 1, 'permissions', 'index'),
+(17, 2, 'users', 'index'),
+(18, 2, 'users', 'search'),
+(19, 2, 'users', 'edit'),
+(20, 2, 'users', 'create'),
+(21, 2, 'profiles', 'index'),
+(22, 2, 'profiles', 'search');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE IF NOT EXISTS `profiles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `active` char(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`id`, `name`, `active`) VALUES
+(1, 'Administrators', 'Y'),
+(2, 'Users', 'Y'),
+(3, 'Read-Only', 'Y');
