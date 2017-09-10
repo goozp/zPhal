7997d693938e58107d63aadde4b4aed2e19eb4eb
gzp <gzp@goozp.com>
2017-09-10 15:07:31 +0800

    兼容Apache

--- app/config/config.php
+++ app/config/config.php
@@ -31,7 +31,7 @@ return new \Phalcon\Config([
         // of the webpspace.  This will break if the public/index.php entry point is moved or
         // possibly if the web server rewrite rules are changed. This can also be set to a static path.
         // 'baseUri'        => preg_replace('/public([\/\\\\])index.php$/', '', $_SERVER["PHP_SELF"]),
-        'baseUri'        => '/',
+        'baseUri'        => './',
     ],
 
     /**
