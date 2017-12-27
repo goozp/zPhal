3f8df55ebe76f9c59b16ea4be2c04e24fb9b44cf
gzp <gzp@goozp.com>
2017-12-28 00:24:42 +0800

    finished archive

--- app/modules/frontend/controllers/ArchivesController.php
+++ app/modules/frontend/controllers/ArchivesController.php
@@ -1,6 +1,8 @@
 <?php
 namespace ZPhal\Modules\Frontend\Controllers;
 
+use ZPhal\Models\Posts;
+
 class ArchivesController extends ControllerBase
 {
     public function initialize()
@@ -25,7 +27,42 @@ class ArchivesController extends ControllerBase
     {
         $this->tag->prependTitle('归档' . " - ");
 
+        $posts = Posts::find([
+            "conditions" => "post_status = 'publish' AND post_type = 'post' ",
+            "columns" => "ID, post_date, post_title, guid, comment_count",
+            "order" => "post_date DESC"
+        ])->toArray();
 
+
+        $arr = [];
+        foreach ($posts as $key => $post){
+            $date = $this->checkDate($post['post_date']);
+            $posts[$key]['theDay'] = $date['day'];
+            $arr[$date['year']][$date['month']][] = $posts[$key];
+        }
+
+        $this->view->setVars([
+            'list' => $arr,
+        ]);
     }
 
+    /**
+     * 检查时间
+     *
+     * @param $time
+     * @return array
+     */
+    protected function checkDate($time)
+    {
+        $timestamp = strtotime($time);
+        $year = date("Y", $timestamp);
+        $month = date("m", $timestamp);
+        $day = date("d", $timestamp);
+
+        return [
+            'year' => $year,
+            'month' => $month,
+            'day' => $day,
+        ];
+    }
 }
