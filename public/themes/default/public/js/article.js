d9d98d603d7031f0bf8711c59ac3955dad27f826
gzp <gzp@goozp.com>
2018-02-02 17:01:18 +0800

    add back-to-top button for default theme

--- public/themes/default/public/js/article.js
+++ public/themes/default/public/js/article.js
@@ -15,4 +15,19 @@ $(document).ready(function() {
 
         tex.find(".katex").css("font-size", "1.6em");
     });
+
+    /* 回到顶部 */
+    var $backToTop = $(".bottom-tools");
+    /* 隐藏回顶部按钮 */
+    $backToTop.hide();
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 200) {
+            $backToTop.fadeIn();
+        } else {
+            $backToTop.fadeOut();
+        }
+    });
+    $backToTop.on('click', function(e) {
+        $("html, body").animate({scrollTop: 0}, 500);
+    });
 });
